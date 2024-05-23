6bbb3039625243a207a2758100349c20a03ccdb7
melikbilyay <37048509+melikbilyay@users.noreply.github.com>
2024-05-23 23:10:51 +0300

    commit

--- Cerroute/components/banner.tsx
+++ Cerroute/components/banner.tsx
@@ -7,6 +7,8 @@ export default function Banner() {
   const [bannerOpen, setBannerOpen] = useState<boolean>(true)
 
   return (
+
+      
       <>
         {bannerOpen && (
             <div className="fixed bottom-0 right-0 w-full md:bottom-8 md:right-12 md:w-auto z-50">
