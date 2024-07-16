70ee38ebb0193e0905d227306cef2a26318260ed
melikbilyay <37048509+melikbilyay@users.noreply.github.com>
2024-07-17 00:21:35 +0300

    Update page.tsx

--- app/(pages)/courses/[courseId]/page.tsx
+++ app/(pages)/courses/[courseId]/page.tsx
@@ -1,13 +1,14 @@
+// components/CourseDetailPage.tsx
 'use client'
-
 import React, { useEffect, useState } from 'react';
-
+import Link from 'next/link';
 import { db } from '@/app/firebaseConfig'; // Adjust path as per your project structure
 import { doc, getDoc } from 'firebase/firestore';
 
 const CourseDetailPage = ({ params }: { params: { courseId: string } }) => {
     const [course, setCourse] = useState<any>(null);
     const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
         if (!params.courseId) return;
@@ -41,6 +42,9 @@ const CourseDetailPage = ({ params }: { params: { courseId: string } }) => {
 
     return (
         <div className="container mx-auto p-4">
+            <Link href="/courses">
+                <span className=" font-semibold text-orange-400 mb-6 inline-block">&lt; Back to Courses</span>
+            </Link>
             <h1 className="text-2xl font-bold mb-4">{course.title}</h1>
             <img className="w-full h-64 object-cover mb-4" src={course.photoURL} alt={course.title} />
             <p className="text-gray-700 text-base">{course.description}</p>
